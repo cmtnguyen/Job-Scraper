d752ba94c9aff9fdd3023bf01bc4152b020f3729
cmtnguyen <66109485+cmtnguyen@users.noreply.github.com>
2024-06-07 19:13:09 -0700

    Update main.py
    Remove exact duplicate results

--- main.py
+++ main.py
@@ -9,16 +9,22 @@ search3 = "Associate Product Owner"
 service = build("customsearch", "v1", developerKey=api_key)
 
 def google_search(search_term):
-    res = (service.cse().list(q=search_term, exactTerms="Product", cx=pse_id, dateRestrict="w[1]", gl="us", siteSearch="lever.co", siteSearchFilter="i")).execute()
+    res = (service.cse().list(q=search_term, exactTerms=search_term, cx=pse_id, dateRestrict="w[1]", gl="us", siteSearch="jobs.lever.co", siteSearchFilter="i")).execute()
     jobs = res['items']
-    res.update((service.cse().list(q=search_term, exactTerms="Product", cx=pse_id, dateRestrict="w[1]", gl="us", siteSearch="greenhouse.io", siteSearchFilter="i")).execute())
+    res.update((service.cse().list(q=search_term, exactTerms=search_term, cx=pse_id, dateRestrict="w[1]", gl="us", siteSearch="boards.greenhouse.io", siteSearchFilter="i")).execute())
     jobs += res['items']
     return jobs
 
-jobs = google_search(search) + google_search(search2) + google_search(search3)
 
-print(f"Generating {len(jobs)} jobs\n")
+jobs = google_search(search) + google_search(search2) + google_search(search3)
+jobSet = []
 for job in jobs:
-    print(job['link'])
-    print(job['title'])
+    jobSet.append([job['link'], job['title']])
+
+jobSet = list(set(map(tuple,jobSet)))
+
+print(f"Generating {len(jobSet)} results\n")
+for job in jobSet:
+    print(job[0])
+    print(job[1])
     print("\n")
